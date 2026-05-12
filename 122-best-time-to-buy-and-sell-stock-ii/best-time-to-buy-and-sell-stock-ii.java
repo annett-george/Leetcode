d9362bf1a6fd76791c2afaeff4fc8e6f6354567f
Annett George <annett.lilly@gmail.com>
2026-05-13 05:04:34 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (82.09%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,17 @@
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
-        for(int i=0; i<prices.length-1; i++){
-            if(prices[i+1]>prices[i]){
-                profit+=prices[i+1]-prices[i];
+        int buy=prices[0];
+        int maxProfit=0;
+        int sum=0;
+        for(int i=1;i<prices.length;i++){
+            if(buy<prices[i]){
+                maxProfit=maxProfit+prices[i]-buy;
+                
             }
+            buy=prices[i];
         }
-        return profit;
+        return maxProfit;
+        
     }
 }
