001d1e40828524ef3cb676bf76f94d309ab38a5c
Annett George <annett.lilly@gmail.com>
2026-05-15 07:27:35 +0530

    Time: 2 ms (61.00%) | Memory: 43.2 MB (37.02%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -2,7 +2,10 @@ class Solution {
     public char findTheDifference(String s, String t) {
         int sum=0;
         for(int i=0; i<s.length(); i++){
-            sum+=t.charAt(i)-s.charAt(i);
+            char ch=t.charAt(i);
+            sum+=ch;
+            ch=s.charAt(i);
+            sum-=ch;
         }
         sum+=t.charAt(t.length()-1);
         return (char)sum;
