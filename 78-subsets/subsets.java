089722c991575d496c34f356c224a3b79c5301d2
Annett George <annett.lilly@gmail.com>
2026-05-19 05:24:54 +0530

    Time: 1 ms (89.06%) | Memory: 44.6 MB (7.41%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        findsubsets(0,nums,new ArrayList<>(),res);
+        return res;
+    }
+    public void findsubsets(int i, int[] arr, List<Integer> list, List<List<Integer>> ans){
+        if(i==arr.length){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        findsubsets(i+1,arr,list,ans);
+        list.add(arr[i]);
+        findsubsets(i+1,arr,list,ans);
+        list.remove(list.size()-1);
+    }
+}
