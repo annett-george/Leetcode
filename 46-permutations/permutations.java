c0e376f586787f5832089907c11e0aa8651de382
Annett George <annett.lilly@gmail.com>
2026-05-19 08:01:08 +0530

    Time: 1 ms (86.89%) | Memory: 44.1 MB (99.97%) - LeetSync

---
+++ 46-permutations/permutations.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void possiblepermutes(int idx, int[] arr, List<List<Integer>> ans){
+        if(idx==arr.length){
+            List<Integer> list = new ArrayList<>();
+            for(int i=0; i<arr.length; i++){
+                list.add(arr[i]);
+            }
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        int temp;
+        for(int i=idx; i<arr.length; i++){
+            temp=arr[idx];
+            arr[idx]=arr[i];
+            arr[i]=temp;
+            possiblepermutes(idx+1,arr,ans);
+            temp=arr[idx];
+            arr[idx]=arr[i];
+            arr[i]=temp;
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        possiblepermutes(0,nums,ans);
+        return ans;
+    }
+}
