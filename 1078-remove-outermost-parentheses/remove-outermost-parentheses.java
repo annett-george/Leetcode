1cefb6a88a8d8ac458558b3d9abda45c1d20018e
Annett George <annett.lilly@gmail.com>
2026-07-29 15:15:36 +0530

    Time: 5 ms (37.27%) | Memory: 44.7 MB (15.60%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -1,21 +1,21 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        String str ="";
+        StringBuilder str = new StringBuilder("");
         int count=0;
         for(Character ch: s.toCharArray()){
             if(ch=='('){
                 if(count!=0){
-                    str+=ch;
+                    str.append(ch);
                 }
                 count--;
             }
             else{
                 count++;
                 if(count!=0){
-                    str+=ch;
+                    str.append(ch);
                 }
             }
         }
-        return str;
+        return str.toString();
     }
 }
