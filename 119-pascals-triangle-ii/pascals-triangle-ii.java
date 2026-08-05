38dc4e21f6c053e4ae6d6da716c18b03cd8d33a6
Annett George <annett.lilly@gmail.com>
2026-08-05 05:37:18 +0530

    Time: 1 ms (70.52%) | Memory: 42.6 MB (56.07%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -3,6 +3,7 @@ class Solution {
         List<Integer> row = new ArrayList<>();
         for(int i=0; i<=rowIndex; i++){
             for(int j=i-1; j>0; j--){
+                // Update backwards to avoid overwriting values needed later 
                 row.set(j, row.get(j)+row.get(j-1));
             }
             row.add(1);
