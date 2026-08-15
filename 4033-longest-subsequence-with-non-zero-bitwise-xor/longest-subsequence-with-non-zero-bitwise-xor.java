ef2917e0c9bedbd8438bc9adb9f537ef64563560
Annett George <annett.lilly@gmail.com>
2026-08-16 05:14:27 +0530

    Time: 2 ms (67.65%) | Memory: 133.6 MB (17.65%) - LeetSync

---
+++ 4033-longest-subsequence-with-non-zero-bitwise-xor/longest-subsequence-with-non-zero-bitwise-xor.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int xor = 0;
+        boolean zero = false;
+
+        for (int num : nums) {
+            xor ^= num;
+
+            if (num != 0) {
+                zero = true;
+            }
+        }
+        if (xor != 0) {
+            return nums.length;
+        }
+        if (zero) {
+            return nums.length - 1;
+        }
+        return 0;
+    }
+}
