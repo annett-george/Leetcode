84825c16f8bdcf3b312a94f37546167402b96214
Annett George <annett.lilly@gmail.com>
2026-09-01 19:40:18 +0530

    Time: 39 ms (5.09%) | Memory: 124.6 MB (11.12%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,14 +3,11 @@ class Solution {
         int m = Integer.MAX_VALUE;
         HashMap<Integer,Integer> hm = new HashMap<>();
         for(int i=0; i<nums.length; i++){
-            if(!hm.containsKey(nums[i])){
-                hm.put(nums[i],i);
-            }
-            else{
-                m = Math.min(m,i-hm.get(nums[i]));
+            if(hm.containsKey(nums[i])){
+                m = i-hm.get(nums[i]);
                 if(m<=k) return true;
-                hm.put(nums[i],i);
             }
+            hm.put(nums[i],i);
         }
         return m<=k;
     }
